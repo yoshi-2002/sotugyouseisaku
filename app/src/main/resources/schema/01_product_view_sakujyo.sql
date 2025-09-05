32eac45a5ea992215acbcd947bf0a03817830009
yos-2002 <kbtjh2002@gmail.com>
2025-09-05 14:09:00 +0900

    ３つの商品テーブルをviewで表示するの完成

---
+++ app/src/main/resources/schema/01_product_view_sakujyo.sql
@@ -0,0 +1,15 @@
+-- ============================
+-- 商品一覧ビュー
+-- ============================
+DROP VIEW IF EXISTS online_shop.product_view;
+
+
+-- DROP TABLE IF EXISTS online_shop.products;
+-- CREATE TABLE online_shop.products (
+--     id SERIAL PRIMARY KEY,
+--     name VARCHAR(100) NOT NULL,
+--     description TEXT,
+--     price NUMERIC(10,2) NOT NULL,
+--     stock INT DEFAULT 0,
+--     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- );
--- app/src/main/resources/schema/05_product_view.sql
+++ app/src/main/resources/schema/05_product_view.sql
@@ -1,6 +1,3 @@
--- ============================
--- 商品一覧ビュー
--- ============================
 DROP VIEW IF EXISTS online_shop.product_view;
 CREATE VIEW online_shop.product_view AS
     SELECT id, name, description, price, stock, created_at, 'smartphone_holder' AS category
@@ -20,14 +17,3 @@ COMMENT ON COLUMN online_shop.product_view.price IS '価格';
 COMMENT ON COLUMN online_shop.product_view.stock IS '在庫数';
 COMMENT ON COLUMN online_shop.product_view.created_at IS '作成日時';
 COMMENT ON COLUMN online_shop.product_view.category IS '商品カテゴリ（smartphone_holder, drive_recorder, car_navigation）';
-
-
--- DROP TABLE IF EXISTS online_shop.products;
--- CREATE TABLE online_shop.products (
---     id SERIAL PRIMARY KEY,
---     name VARCHAR(100) NOT NULL,
---     description TEXT,
---     price NUMERIC(10,2) NOT NULL,
---     stock INT DEFAULT 0,
---     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
--- );

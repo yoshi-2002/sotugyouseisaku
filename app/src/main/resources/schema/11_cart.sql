840c8dba929488f68e2133cf30be6917b5f49de7
yos-2002 <kbtjh2002@gmail.com>
2025-09-18 15:26:53 +0900

    数の修正完了

--- app/src/main/resources/schema/11_cart.sql
+++ app/src/main/resources/schema/11_cart.sql
@@ -1,10 +1,11 @@
 CREATE TABLE online_shop.cart (
     id SERIAL PRIMARY KEY,
-    product_id INT NOT NULL,               -- 商品ID（外部キー）
-    quantity INT DEFAULT 1,                -- カートに入れた数量
-    added_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- カートに追加した日時
+    product_id INT NOT NULL UNIQUE,        -- 商品ID（外部キー, ユニーク制約）
+    quantity INT DEFAULT 1,                -- 数量
+    added_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT fk_cart_product FOREIGN KEY (product_id) REFERENCES online_shop.product(id)
 );
+
 
 COMMENT ON TABLE online_shop.cart IS '簡易カートテーブル（ユーザー無し、一人用）';
 COMMENT ON COLUMN online_shop.cart.id IS 'カートID';
